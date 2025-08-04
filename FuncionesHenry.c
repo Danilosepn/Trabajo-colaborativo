91dfadbde59710bd0b8335470935b2d37aa6e086
Henry-derv <henry.luisa@epn.edu.ec>
2025-08-03 21:47:08 -0500

    Add files via upload

---
+++ FuncionesHenry.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "funciones.h"
+
+void saludo() {
+	printf("Hola, soy Henry y este es mi primer cambio.\n");
+}
+
+void suma(int a, int b) {
+	printf("La suma es: %d\n", a + b);
+}
