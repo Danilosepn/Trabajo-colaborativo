31f52cebc9639e157247b8a4266237065aea8445
Danilosepn <danilo.unapucha@epn.edu.ec>
2025-08-03 20:48:45 -0500

    Add files via upload

---
+++ funciones .h
@@ -0,0 +1,4 @@
+#ifndef FUNCIONES _H
+#define FUNCIONES _H
+
+#endif
---
+++ main .cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "funciones.h"
+
+int main() {
+	printf("=== Proyecto Colaborativo ===\n");
+	saludo();
+	suma(5, 3);
+	return 0;
+}
