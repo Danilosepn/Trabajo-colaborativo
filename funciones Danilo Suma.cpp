4046483e5f0a255f11646b8e913d04264906d3a8
Danilosepn <danilo.unapucha@epn.edu.ec>
2025-08-03 20:59:49 -0500

    Agregar un nuevo mensaje en suma():
    se le agrega el mensaje de "Calculando la suma..."

---
+++ funciones Danilo Suma.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "funciones.h"
+
+void saludo() {
+	printf("Hola, soy Danilo y este es mi primer cambio.\n");
+}
+
+void suma(int a, int b) {
+	printf("Calculando la suma...\n");
+	printf("La suma es: %d\n", a + b);
+}
